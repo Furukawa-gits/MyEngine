ccd20ef4420f5731915216f739292b7966445ddf
Kouki Furukawa <k020g1198@gmail.com>
2022-10-23 22:49:41 +0900

    ウィンドウ名変更

--- Base/WindowGenerate.cpp
+++ Base/WindowGenerate.cpp
@@ -37,7 +37,7 @@ void Window_Generate::Win_Create()
 	//ウィンドウ生成
 	hwnd = CreateWindow(
 		w.lpszClassName,
-		L"DirectXGame",
+		L"DragShoot",
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT,
 		CW_USEDEFAULT,
