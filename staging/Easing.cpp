96a298f709050aabaec5d8ef037c286dab84e9a9
Kouki Furukawa <k020g1198@gmail.com>
2022-11-02 15:14:45 +0900

    イージングクラス実装

--- staging/Easing.cpp
+++ staging/Easing.cpp
@@ -8,13 +8,13 @@ easingManager::~easingManager()
 {
 }
 
-void easingManager::set(easingPattern pattaern, easingLevel level, int totaltime, float start, float end)
+void easingManager::set(easingType pattaern, easingPattern type, int totaltime, float start, float end)
 {
-	thisPattern = pattaern;
-	thisLevel = level;
+	thisType = pattaern;
+	thisPattern = type;
 	totalFrame = totaltime;
 	this->start = start;
-	this->end = end;
+	this->lange = end - start;
 	frame = 0.0f;
 	rate = 0.0f;
 	isActive = true;
@@ -24,63 +24,367 @@ float easingManager::easing()
 {
 	if (!isActive)
 	{
-		return 0.0f;
+		return start;
 	}
 
+	//フレーム加算
+	frame++;
 
+	//進行度の割合を算出
+	rate = frame / totalFrame;
+
+	//イーズイン
+	easeIn();
+
+	//イーズアウト
+	easeOut();
+
+	//イーズインアウト
+	easeInOut();
+
+	if (frame == totalFrame)
+	{
+		easingNum = start + lange;
+		isActive = false;
+	}
 
 	return easingNum;
 }
 
 void easingManager::easeIn()
 {
-	if (thisPattern != easingPattern::easeIn)
+	if (thisType != easingType::easeIn)
 	{
 		return;
 	}
+
+	inQuadratic();
+	inCubic();
+	inQuartic();
+	inQuintic();
+	inSinusoidal();
+	inExponential();
+	inCircular();
 }
 
 void easingManager::easeOut()
 {
-	if (thisPattern != easingPattern::easeOut)
+	if (thisType != easingType::easeOut)
 	{
 		return;
 	}
+
+	outQuadratic();
+	outCubic();
+	outQuartic();
+	outQuintic();
+	outSinusoidal();
+	outExponential();
+	outCircular();
 }
 
 void easingManager::easeInOut()
 {
-	if (thisPattern != easingPattern::easeInOut)
+	if (thisType != easingType::easeInOut)
 	{
 		return;
 	}
+
+	inOutQuadratic();
+	inOutCubic();
+	inOutQuartic();
+	inOutQuintic();
+	inOutSinusoidal();
+	inOutExponential();
+	inOutCircular();
 }
 
+#pragma region イーズイン
 void easingManager::inQuadratic()
 {
+	if (thisPattern != easingPattern::Quadratic)
+	{
+		return;
+	}
+
+	easingNum = lange * rate * rate + start;
 }
 
 void easingManager::inCubic()
 {
+	if (thisPattern != easingPattern::Cubic)
+	{
+		return;
+	}
+
+	easingNum = lange * rate * rate * rate + start;
 }
 
 void easingManager::inQuartic()
 {
+	if (thisPattern != easingPattern::Quartic)
+	{
+		return;
+	}
+
+	easingNum = lange * rate * rate * rate * rate + start;
 }
 
 void easingManager::inQuintic()
 {
+	if (thisPattern != easingPattern::Quintic)
+	{
+		return;
+	}
+
+	easingNum = lange * rate * rate * rate * rate * rate + start;
 }
 
 void easingManager::inSinusoidal()
 {
+	if (thisPattern != easingPattern::Sinusoidal)
+	{
+		return;
+	}
+
+	easingNum = -lange * cosf(rate * (M_PI / 2.0f)) + start;
 }
 
 void easingManager::inExponential()
 {
+	if (thisPattern != easingPattern::Exponential)
+	{
+		return;
+	}
+
+	easingNum = lange * powf(2, (10 * (rate - 1))) + start;
 }
 
 void easingManager::inCircular()
 {
+	if (thisPattern != easingPattern::Circular)
+	{
+		return;
+	}
+
+	easingNum = -lange * (sqrtf(1 - rate * rate) - 1) + start;
+}
+#pragma endregion
+
+#pragma region イーズアウト
+void easingManager::outQuadratic()
+{
+	if (thisPattern != easingPattern::Quadratic)
+	{
+		return;
+	}
+
+	easingNum = -lange * rate * (rate - 2.0f) + start;
 }
 
+void easingManager::outCubic()
+{
+	if (thisPattern != easingPattern::Cubic)
+	{
+		return;
+	}
+
+	rate = rate - 1.0f;
+
+	easingNum = lange * (rate * rate * rate + 1.0f) + start;
+}
+
+void easingManager::outQuartic()
+{
+	if (thisPattern != easingPattern::Quartic)
+	{
+		return;
+	}
+
+	rate = rate - 1.0f;
+
+	easingNum = -lange * (rate * rate * rate * rate - 1.0f) + start;
+}
+
+void easingManager::outQuintic()
+{
+	if (thisPattern != easingPattern::Quintic)
+	{
+		return;
+	}
+
+	rate = rate - 1.0f;
+
+	easingNum = lange * (rate * rate * rate * rate * rate + 1.0f) + start;
+}
+
+void easingManager::outSinusoidal()
+{
+	if (thisPattern != easingPattern::Sinusoidal)
+	{
+		return;
+	}
+
+	easingNum = rate * sinf(rate * (M_PI / 2.0f)) + start;
+}
+
+void easingManager::outExponential()
+{
+	if (thisPattern != easingPattern::Exponential)
+	{
+		return;
+	}
+
+	easingNum = rate * (-(powf(2.0f, -10.0f * rate)) + 1) + start;
+}
+
+void easingManager::outCircular()
+{
+	if (thisPattern != easingPattern::Circular)
+	{
+		return;
+	}
+
+	rate = rate - 1.0f;
+
+	easingNum = lange * sqrtf(1 - rate * rate) + start;
+}
+#pragma endregion
+
+#pragma region イーズインアウト
+void easingManager::inOutQuadratic()
+{
+	if (thisPattern != easingPattern::Quadratic)
+	{
+		return;
+	}
+
+	rate *= 2.0f;
+
+	if (rate < 1.0f)
+	{
+		easingNum = lange / 2.0f * rate * rate + start;
+	}
+	else
+	{
+		rate = rate - 1;
+
+		easingNum = -lange / 2.0f * (rate * (rate - 2) - 1) + start;
+	}
+}
+
+void easingManager::inOutCubic()
+{
+	if (thisPattern != easingPattern::Cubic)
+	{
+		return;
+	}
+
+	rate *= 2.0f;
+
+	if (rate < 1.0f)
+	{
+		easingNum = lange / 2.0f * rate * rate * rate + start;
+	}
+	else
+	{
+		rate = rate - 2;
+
+		easingNum = lange / 2.0f * (rate * rate * rate + 2) + start;
+	}
+}
+
+void easingManager::inOutQuartic()
+{
+	if (thisPattern != easingPattern::Quartic)
+	{
+		return;
+	}
+
+	rate *= 2.0f;
+
+	if (rate < 1.0f)
+	{
+		easingNum = lange / 2.0f * rate * rate * rate * rate + start;
+	}
+	else
+	{
+		rate = rate - 2;
+
+		easingNum = -lange / 2.0f * (rate * rate * rate * rate - 2) + start;
+	}
+}
+
+void easingManager::inOutQuintic()
+{
+	if (thisPattern != easingPattern::Quintic)
+	{
+		return;
+	}
+
+	rate *= 2.0f;
+
+	if (rate < 1.0f)
+	{
+		easingNum = lange / 2.0f * rate * rate * rate * rate * rate + start;
+	}
+	else
+	{
+		rate = rate - 2;
+
+		easingNum = lange / 2.0f * (rate * rate * rate * rate * rate + 2) + start;
+	}
+}
+
+void easingManager::inOutSinusoidal()
+{
+	if (thisPattern != easingPattern::Sinusoidal)
+	{
+		return;
+	}
+
+	easingNum = -lange / 2.0f * (cosf(M_PI * rate) - 1) + start;
+}
+
+void easingManager::inOutExponential()
+{
+	if (thisPattern != easingPattern::Exponential)
+	{
+		return;
+	}
+
+	rate *= 2.0f;
+
+	if (rate < 1.0f)
+	{
+		easingNum = lange / 2.0f * powf(2.0f, 10.0f * (rate - 1)) + start;
+	}
+	else
+	{
+		rate = rate - 1;
+
+		easingNum = lange / 2.0f * (-(powf(2.0f, -10.0f * rate)) + 2) + start;
+	}
+}
+
+void easingManager::inOutCircular()
+{
+	if (thisPattern != easingPattern::Circular)
+	{
+		return;
+	}
+
+	rate *= 2.0f;
+
+	if (rate < 1.0f)
+	{
+		easingNum = -lange / 2.0f * sqrtf(1 - rate * rate) + start;
+	}
+	else
+	{
+		rate = rate - 2;
+
+		easingNum = lange / 2.0f * (sqrtf(1 - rate * rate) + 1) + start;
+	}
+}
+#pragma endregion
--- staging/Easing.h
+++ staging/Easing.h
@@ -1,13 +1,15 @@
 #pragma once
+#define _USE_MATH_DEFINES
+#include<math.h>
 
-enum class easingPattern
+enum class easingType
 {
 	easeIn = 0,
 	easeOut = 1,
 	easeInOut = 2
 };
 
-enum class easingLevel
+enum class easingPattern
 {
 	Quadratic = 0,
 	Cubic = 1,
@@ -26,8 +28,15 @@ public:
 	easingManager();
 	~easingManager();
 
-	//イージングのセット
-	void set(easingPattern pattaern, easingLevel level, int totaltime, float start, float end);
+	/// <summary>
+	/// イージングのセット
+	/// </summary>
+	/// <param name="pattaern">イージングパターン</param>
+	/// <param name="type">イージングタイプ</param>
+	/// <param name="totaltime">イージング全体のフレーム</param>
+	/// <param name="start">初期位置</param>
+	/// <param name="end">最終位置</param>
+	void set(easingType pattaern, easingPattern type, int totaltime, float start, float end);
 
 	/// <summary>
 	/// <para>イージング</para>
@@ -47,18 +56,28 @@ private:
 	void easeInOut();
 
 	void inQuadratic();
-
 	void inCubic();
-
 	void inQuartic();
-
 	void inQuintic();
-
 	void inSinusoidal();
-
 	void inExponential();
-
 	void inCircular();
+
+	void outQuadratic();
+	void outCubic();
+	void outQuartic();
+	void outQuintic();
+	void outSinusoidal();
+	void outExponential();
+	void outCircular();
+
+	void inOutQuadratic();
+	void inOutCubic();
+	void inOutQuartic();
+	void inOutQuintic();
+	void inOutSinusoidal();
+	void inOutExponential();
+	void inOutCircular();
 
 
 private:
@@ -77,15 +96,15 @@ private:
 	//初期値
 	float start = 0;
 
-	//最終値
-	float end = 0;
+	//移動量
+	float lange = 0;
 
 	//イージングの値(返り値用)
 	float easingNum = 0;
 
 	//イージングの種類
-	easingPattern thisPattern;
+	easingType thisType;
 
 	//イージングのレベル
-	easingLevel thisLevel;
+	easingPattern thisPattern;
 };
