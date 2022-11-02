601363bed29a5a16dc96106c1440f0e0ab7076ac
Kouki Furukawa <k020g1198@gmail.com>
2022-11-02 11:46:45 +0900

    regionでまとめた

--- Player/CheckHItManager.h
+++ Player/CheckHItManager.h
@@ -10,6 +10,7 @@ using namespace std;
 //プレイヤーと敵の弾の当たり判定
 //プレイヤーと敵のロックオン判定
 
+#pragma region プレイヤーと敵本体の当たり判定
 /// <summary>
 /// プレイヤーと敵本体の当たり判定(単体)
 /// </summary>
@@ -23,7 +24,9 @@ void checkPlayerEnemy(Player* player,Enemy* enemy);
 /// <param name="player">プレイヤー</param>
 /// <param name="enemys">敵(群れ)</param>
 void checkPlayerEnemys(Player* player, vector<Enemy>* enemys);
+#pragma endregion
 
+#pragma region プレイヤーの通常弾と敵の当たり判定
 /// <summary>
 /// プレイヤーの通常弾と敵の当たり判定(単体)
 /// </summary>
@@ -37,13 +40,17 @@ void checkBulletEnemy(vector<bullet>* bullets, Enemy* enemy);
 /// <param name="bullets">プレイヤーの通常弾</param>
 /// <param name="enemys">敵(群れ)</param>
 void checkBulletEnemy(vector<bullet>* bullets, vector<Enemy>* enemys);
+#pragma endregion 
 
+#pragma region プレイヤーの追尾弾と敵の当たり判定
 /// <summary>
 /// プレイヤーの追尾弾と敵の当たり判定
 /// </summary>
 /// <param name="missiles">プレイヤーの追尾弾</param>
 void checkHomingEnemy(vector<Missile>* missiles);
+#pragma endregion
 
+#pragma region プレイヤーと敵の弾の当たり判定
 /// <summary>
 /// プレイヤーと敵の弾の当たり判定(単体)
 /// </summary>
@@ -57,7 +64,9 @@ void chackPlayerEnemyBullet(Player* player, Enemy* enemy);
 /// <param name="player">プレイヤー</param>
 /// <param name="enemys">敵(群れ)</param>
 void checkPlayerEnemyBullets(Player* player, vector<Enemy>* enemys);
+#pragma endregion
 
+#pragma region プレイヤーと敵のロックオン判定
 /// <summary>
 /// プレイヤーと敵のロックオン判定(単体)
 /// </summary>
@@ -71,3 +80,4 @@ void checkRockonEnemy(Player* player, Enemy* enemy);
 /// <param name="player"></param>
 /// <param name="enemys"></param>
 void checkRockonEnemys(Player* player, vector<Enemy>* enemys);
+#pragma endregion
