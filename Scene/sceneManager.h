79fb0c3e92c9e504d653415f111d03a6d8503b82
Kouki Furukawa <k020g1198@gmail.com>
2023-05-09 02:27:28 +0900

    sceneManager準備

---
+++ Scene/sceneManager.h
@@ -0,0 +1,23 @@
+#pragma once
+#include"titleScene.h"
+#include"selectScene.h"
+#include"playScene.h"
+#include"resultScene.h"
+
+class sceneManager
+{
+public:
+	sceneManager();
+
+	//ƒV[ƒ“‚Ì•t‚¯‘Ö‚¦
+	void replacementScene();
+
+public:
+	primitiveScene* nowscene;
+
+private:
+	titleScene title;
+	selectScene select;
+	playScene play;
+	resultScene result;
+};
