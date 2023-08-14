dc595e98ac571a4d69b3d507093219a387041943
Kouki Furukawa <k020g1198@gmail.com>
2023-08-14 16:53:10 +0900

    ファイル追加

---
+++ Enemy/UniteBossMainBody.cpp
@@ -0,0 +1,53 @@
+#include "UniteBossMainBody.h"
+
+uniteBossMainBody::uniteBossMainBody()
+{
+}
+
+uniteBossMainBody::~uniteBossMainBody()
+{
+}
+
+void uniteBossMainBody::init(bool isboss)
+{
+}
+
+void uniteBossMainBody::set(XMFLOAT3 pos)
+{
+}
+
+void uniteBossMainBody::updata()
+{
+}
+
+void uniteBossMainBody::arrival()
+{
+}
+
+void uniteBossMainBody::ariveMove()
+{
+}
+
+void uniteBossMainBody::deathMove()
+{
+}
+
+void uniteBossMainBody::deathMoveBoss()
+{
+}
+
+void uniteBossMainBody::updataSprite()
+{
+}
+
+void uniteBossMainBody::draw3D()
+{
+}
+
+void uniteBossMainBody::draw2D()
+{
+}
+
+void uniteBossMainBody::drawMiniMapIcon()
+{
+}
---
+++ Enemy/UniteBossSubParts.cpp
@@ -0,0 +1,53 @@
+#include "UniteBossSubParts.h"
+
+uniteBossSubParts::uniteBossSubParts()
+{
+}
+
+uniteBossSubParts::~uniteBossSubParts()
+{
+}
+
+void uniteBossSubParts::init(bool isboss)
+{
+}
+
+void uniteBossSubParts::set(XMFLOAT3 pos)
+{
+}
+
+void uniteBossSubParts::updata()
+{
+}
+
+void uniteBossSubParts::arrival()
+{
+}
+
+void uniteBossSubParts::ariveMove()
+{
+}
+
+void uniteBossSubParts::deathMove()
+{
+}
+
+void uniteBossSubParts::deathMoveBoss()
+{
+}
+
+void uniteBossSubParts::updataSprite()
+{
+}
+
+void uniteBossSubParts::draw3D()
+{
+}
+
+void uniteBossSubParts::draw2D()
+{
+}
+
+void uniteBossSubParts::drawMiniMapIcon()
+{
+}
